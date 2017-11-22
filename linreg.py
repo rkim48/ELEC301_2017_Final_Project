a568c1327a6d75d5fb2fbc2b9c45b4a22d8c4686
yeetboi123 <rk48@rice.edu>
2017-11-21 19:47:24 -0600

    Added Linear Regression

---
+++ linreg.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('TrainingMetadata.csv')
+unlabeled_dataset = pd.read_csv('UnlabeledTestMetadata.csv')
+X = dataset.drop(['Type','Number'],axis = 1)
+y = dataset.iloc[:,1].values
+X_final = unlabeled_dataset.drop(['Number'], axis = 1)
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, \
+                                                    random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+from sklearn.metrics import r2_score
+score = r2_score(y_test, y_pred)
+print(score)
